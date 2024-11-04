5c2a6e178c6f63024e836aad838485f21e19dc13
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-11-04 14:46:52 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+    LOG_GROUP = os.getenv("LOG_GROUP", "cdms-log-group")
+    REGION = os.getenv("AWS_REGION", "us-west-2")

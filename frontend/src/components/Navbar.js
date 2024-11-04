91d87e167a43bc76dad53f87edf3b7068d2ba9c2
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-11-04 15:35:48 +0300

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">CDMS</a>
+            <Link className="navbar-brand" to="/">CDMS</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>

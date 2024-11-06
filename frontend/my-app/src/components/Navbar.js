e0408fac80934d796c346f7d859d8cedee05bd76
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-11-06 15:44:15 +0300

    Create Navbar.js

---
+++ frontend/my-app/src/components/Navbar.js
@@ -0,0 +1,32 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">CDMS</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/report-incident">Report Incident</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register-volunteer">Volunteer</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

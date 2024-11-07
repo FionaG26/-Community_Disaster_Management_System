1380bdd6b6dd90298cc2db7147c9190cdf565287
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-11-07 09:55:30 +0300

    Create Hero.js

---
+++ frontend/my-app/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => (
+  <section className="hero">
+    <h1>Community Disaster Management System</h1>
+    <p>Providing help and support during times of need</p>
+    <button className="cta-button">Get Involved</button>
+  </section>
+);
+
+export default Hero;

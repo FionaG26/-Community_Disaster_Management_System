85a60c47ad3de154f16c34ef4cb11e6697b162e6
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-11-07 10:26:59 +0300

    Update Volunteer.js

--- frontend/my-app/src/components/Volunteer.js
+++ frontend/my-app/src/components/Volunteer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import VolunteerRegistration from './VolunteerRegistration'; // Import VolunteerRegistration
 
 const Volunteer = () => (
   <section id="volunteer" className="container py-5">

8af64a9cf6448102763498302554ae49d1999d46
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-11-07 10:02:09 +0300

    Create Footer.js

---
+++ frontend/my-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="text-center py-4">
+    <p>&copy; 2024 Community Disaster Management System | All rights reserved</p
+  </footer>
+);
+
+export default Footer;
